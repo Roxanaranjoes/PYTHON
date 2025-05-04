ece086ae148c34700fb8d3bb580607b4794991b6
Roxana Naranjo <rnaranjo@unal.edu.co>
2025-05-04 08:33:03 -0500

    Actualizando contenido Semana_2

--- MODULO_1/Semana_1/Conducir.py
+++
@@ -1,9 +0,0 @@
-#Conducir
-
-edad = int(input("¿Cuál es tu edad? "))
-licencia = input("¿Tienes licencia de conducción? (si/no): ").lower()
-
-if edad >= 18 and licencia == "si":
-    print("Puedes conducir")
-else:
-    print("No puedes conducir")
--- MODULO_1/Semana_1/Laboral.py
+++
@@ -1,9 +0,0 @@
-#Experiencia Laboral y titulo universitario
-
-experiencia_laboral = input("¿Tienes experiencia laboral? (si/no): ").lower()
-titulo_universitario = input("¿Tienes título universitario? (si/no): ").lower()
-
-if experiencia_laboral == "si" and titulo_universitario == "si":
-    print("Puedes aplicar a la oferta de trabajo")
-else:
-    print("No cumples con los requisitos")
